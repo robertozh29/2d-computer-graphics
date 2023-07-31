9baa63f320fc7fb90d2895d1519bb27c87d63437
Roberto Zepeda Hipolito <robertozh29hotmail.com>
2023-07-31 10:33:53 -0600

    Classe Algorithm y sus funciones

--- Root.py
+++ Root.py
@@ -28,7 +28,7 @@ class Root(customtkinter.CTk):
         self.sidebar.grid(column=0, row=0, sticky="NS")
         
         # ----- main_frame -----
-        self.main_frame = MainFrame(self, "circulo_DDA")
+        self.main_frame = MainFrame(self, "linea_DDA")
         self.plot(self.main_frame.frame_chart)
 
     def linea_bresenham(self):
@@ -54,6 +54,11 @@ class Root(customtkinter.CTk):
     def elipse_punto_medio(self):
         self.main_frame.destroy()
         self.main_frame = MainFrame(self, "elipse_punto_medio")
+        self.plot(self.main_frame.frame_chart)
+    
+    def parabola(self):
+        self.main_frame.destroy()
+        self.main_frame = MainFrame(self, "parabola")
         self.plot(self.main_frame.frame_chart)
 
     def plot(self, frame):
@@ -120,7 +125,7 @@ class SideBarFrame(customtkinter.CTkFrame):
         self.button5 = customtkinter.CTkButton(self, text="Elipse por punto medio", command=root.elipse_punto_medio)
         self.button5.grid(column=0, row=5, padx=30, pady=7, ipadx=46)
 
-        self.button6 = customtkinter.CTkButton(self, text="Parabola", command=root.destroy)
+        self.button6 = customtkinter.CTkButton(self, text="Parabola", command=root.parabola)
         self.button6.grid(column=0, row=6, padx=30, pady=7, ipadx=52)
 
         self.button7 = customtkinter.CTkButton(self, text="Clear all", command=root.destroy, fg_color="red", hover_color="#b20101")
@@ -194,7 +199,7 @@ class AlgorithmFrame(customtkinter.CTkFrame):
             Yc = customtkinter.CTkEntry(self, placeholder_text="Y Central")
             Yc.grid(column=2, row=1, padx=10, pady=(20,40), sticky="WE")
 
-        elif(algorithm == "ciruclo_punto_medio"):
+        elif(algorithm == "circulo_punto_medio"):
             titulo_inputs = customtkinter.CTkLabel(self, text="Circulo por punto medio", font=("helvetica", 20))
             titulo_inputs.grid(row=0, column=0, columnspan=3, pady=20, sticky="WE")
 
@@ -223,6 +228,223 @@ class AlgorithmFrame(customtkinter.CTkFrame):
             Ry = customtkinter.CTkEntry(self, placeholder_text="Radio Y")
             Ry.grid(column=2, row=1, padx=10, pady=(20,40), sticky="WE")
 
+        elif(algorithm == "parabola"):
+            titulo_inputs = customtkinter.CTkLabel(self, text="Parabola", font=("helvetica", 20))
+            titulo_inputs.grid(row=0, column=0, columnspan=4,  pady=20, sticky="EW")
+
+            x0 = customtkinter.CTkEntry(self, placeholder_text="X0")
+            x0.grid(column=0, row=1, padx=(20,10), pady=(20,40), sticky="WE")
+
+            y0 = customtkinter.CTkEntry(self, placeholder_text="Y0")
+            y0.grid(column=1, row=1, padx=10, pady=(20,40), sticky="WE")
+            
+            x1 = customtkinter.CTkEntry(self, placeholder_text="X1")
+            x1.grid(column=2, row=1, padx=10, pady=(20,40), sticky="WE")
+
+            y1 = customtkinter.CTkEntry(self, placeholder_text="Y1")
+            y1.grid(column=3, row=1, padx=(10,20), pady=(20,40), sticky="WE")
+
+class Algorithm():
+    def __init__(self, root):
+        super().__init__(root)
+    
+    def lineDDA(x0, y0, x1, y1):
+        points = [] # Lista para almacenar los puntos de la línea
+        dx = x1 - x0 # Diferencia en x
+        dy = y1 - y0 # Diferencia en y
+
+        # Determinar el número de pasos basado en la diferencia más grande entre x y y
+        if abs(dx) > abs(dy):
+            steps = abs(dx)
+        else:
+            steps = abs(dy)
+
+        Xinc = dx / steps # Incremento en x
+        Yinc = dy / steps # Incremento en y
+
+        X = x0 # Valor inicial de x
+        Y = y0 # Valor inicial de y
+
+        points.append((round(X), round(Y))) # Agregar el primer punto
+
+        # Loop para calcular los puntos de la línea
+        for _ in range(steps):
+            X += Xinc
+            Y += Yinc
+            points.append((round(X), round(Y)))
+
+        return points
+
+    # Función para dibujar una línea usando el algoritmo de Bresenham
+    def lineBresenham(x0, y0, x1, y1):
+        points = [] # Lista para almacenar los puntos de la línea
+        dx = abs(x1 - x0) # Diferencia absoluta en x
+        dy = abs(y1 - y0) # Diferencia absoluta en y
+
+        # Determinar la dirección de los incrementos en x y y
+        sx = 1 if x0 < x1 else -1
+        sy = 1 if y0 < y1 else -1
+
+        err = dx - dy
+
+        # Loop para calcular los puntos de la línea
+        while True:
+            points.append((x0, y0))
+            if x0 == x1 and y0 == y1:
+                break
+
+            e2 = 2 * err
+            if e2 > -dy:
+                err -= dy
+                x0 += sx
+            if e2 < dx:
+                err += dx
+                y0 += sy
+
+        return points
+
+    # Función para dibujar un círculo usando el algoritmo DDA
+    def circleDDA(xc, yc, r):
+        points = [] # Lista para almacenar los puntos del círculo
+        X = r
+        Y = 0
+
+        # Agregar puntos iniciales en los ejes
+        points.append((xc + X, yc + Y))
+        points.append((xc - X, yc - Y))
+        points.append((xc + Y, yc + X))
+        points.append((xc - Y, yc + X))
+
+        P = 1 - r # Parámetro inicial
+
+        # Loop para calcular los puntos del círculo en el primer octante
+        while X > Y:
+            Y += 1
+            if P <= 0:
+                P = P + 2 * Y + 1
+            else:
+                X -= 1
+                P = P + 2 * Y - 2 * X + 1
+
+            # Agregar puntos en todos los octantes
+            points.append((xc + X, yc + Y))
+            points.append((xc - X, yc + Y))
+            points.append((xc + X, yc - Y))
+            points.append((xc - X, yc - Y))
+            points.append((xc + Y, yc + X))
+            points.append((xc - Y, yc + X))
+            points.append((xc + Y, yc - X))
+            points.append((xc - Y, yc - X))
+
+        return points
+
+    # Función para dibujar un círculo usando el algoritmo de punto medio
+    def circleMidPoint(xc, yc, r):
+        points = [] # Lista para almacenar los puntos del círculo
+        x = 0
+        y = r
+        P = 1 - r # Parámetro inicial
+
+        # Agregar puntos iniciales en los ejes
+        points.append((xc + x, yc + y))
+        points.append((xc - x, yc + y))
+        points.append((xc + x, yc - y))
+        points.append((xc - x, yc - y))
+        points.append((xc + y, yc + x))
+        points.append((xc - y, yc + x))
+        points.append((xc + y, yc - x))
+        points.append((xc - y, yc - x))
+
+        # Loop para calcular los puntos del círculo en el primer octante
+        while x < y:
+            x += 1
+            if P < 0:
+                P = P + 2 * x + 1
+            else:
+                y -= 1
+                P = P + 2 * x - 2 * y + 1
+
+            # Agregar puntos en todos los octantes
+            points.append((xc + x, yc + y))
+            points.append((xc - x, yc + y))
+            points.append((xc + x, yc - y))
+            points.append((xc - x, yc - y))
+            points.append((xc + y, yc + x))
+            points.append((xc - y, yc + x))
+            points.append((xc + y, yc - x))
+            points.append((xc - y, yc - x))
+
+        return points
+
+    # Función para dibujar una elipse usando el algoritmo de punto medio
+    def ellipseMidPoint(xc, yc, rx, ry):
+        points = [] # Lista para almacenar los puntos de la elipse
+        x = 0
+        y = ry
+        P1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx) # Parámetro inicial en la región 1
+        dx = 2 * ry * ry * x
+        dy = 2 * rx * rx * y
+
+        # Loop para calcular los puntos de la elipse en la región 1
+        while dx < dy:
+            points.append((xc + x, yc + y))
+            points.append((xc - x, yc + y))
+            points.append((xc + x, yc - y))
+            points.append((xc - x, yc - y))
+
+            if P1 < 0:
+                x += 1
+                dx = dx + (2 * ry * ry)
+                P1 = P1 + dx + (ry * ry)
+            else:
+                x += 1
+                y -= 1
+                dx = dx + (2 * ry * ry)
+                dy = dy - (2 * rx * rx)
+                P1 = P1 + dx - dy + (ry * ry)
+
+        P2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) + ((rx * rx) * ((y - 1) * (y - 1))) - (rx * rx * ry * ry) # Parámetro inicial en la región 2
+
+        # Loop para calcular los puntos de la elipse en la región 2
+        while y >= 0:
+            points.append((xc + x, yc + y))
+            points.append((xc - x, yc + y))
+            points.append((xc + x, yc - y))
+            points.append((xc - x, yc - y))
+
+            if P2 > 0:
+                y -= 1
+                dy = dy - (2 * rx * rx)
+                P2 = P2 + (rx * rx) - dy
+            else:
+                y -= 1
+                x += 1
+                dx = dx + (2 * ry * ry)
+                dy = dy - (2 * rx * rx)
+                P2 = P2 + dx - dy + (rx * rx)
+
+        return points
+
+    # Calcular coordenadas parabola
+    def parabola(x0, y0, x1, y1):
+        points = [] # Lista para almacenar los puntos de la parábola
+        dx = x1 - x0 # Diferencia en x
+        dy = y1 - y0 # Diferencia en y
+        y = y0
+        p = dy - dx / 2 # Parámetro inicial
+
+        # Loop para calcular los puntos de la parábola
+        while y <= y1:
+            points.append((x0, y))
+            if p < 0:
+                x0 += 1
+                p = p + dy
+            else:
+                x0 += 1
+                y += 1
+                p = p + dy - dx
+
+        return points
 
 
 ventana = Root()
